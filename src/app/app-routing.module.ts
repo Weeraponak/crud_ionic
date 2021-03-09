ded580d107a08a9101db6692f67256bbc6476848
weeraponak <weerapon.ak.61@ubu.ac.th>
2021-03-10 02:11:02 +0700

    ok know

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'edit-post/:id',
+    loadChildren: () => import('./edit-post/edit-post.module').then( m => m.EditPostPageModule)
+  },
+  {
+    path: 'add-post',
+    loadChildren: () => import('./add-post/add-post.module').then( m => m.AddPostPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyDq2Hli3HjSAUOetsp5fd5EQ1Nh1SzGBiU",
+    authDomain: "myionicdb632.firebaseapp.com",
+    databaseURL: "https://myionicdb632-default-rtdb.firebaseio.com",
+    projectId: "myionicdb632",
+    storageBucket: "myionicdb632.appspot.com",
+    messagingSenderId: "1012482443858",
+    appId: "1:1012482443858:web:440db4ee5bbea8220fed8b",
+    measurementId: "G-WN8NBPYT28"
+  }
+};
